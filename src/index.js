c14737ec7f1c347149b8bf9acea3b3f4f31aa0d3
minsukwon <libertyfromthinking@gmail.com>
2023-03-09 02:57:21 +0900

    바닐라스크립트 todolist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const form = document.querySelector("form");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-);
+import { legacy_createStore as createStore } from "redux";
+
+const ADD = "add";
+const DELETE = "delete";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter((todo) => {
+        console.log(`actionid : ${typeof action.id}`);
+        console.log(`todo id: ${typeof todo.id}`);
+        return todo.id !== action.id;
+      });
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+const onClick = (event) => {
+  event.preventDefault();
+  store.dispatch({ type: ADD, text: input.value });
+  input.value = "";
+  console.log(store.getState());
+  updateTodos();
+};
+
+const onDelete = (event) => {
+  event.preventDefault();
+  const id = parseInt(event.target.parentNode.id);
+  store.dispatch({ type: DELETE, id });
+  updateTodos();
+};
+
+form.addEventListener("submit", onClick);
+
+const updateTodos = () => {
+  const todos = store.getState();
+  ul.innerHTML = "";
+  todos.map((todo) => {
+    const li = document.createElement("li");
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "remove";
+    removeBtn.addEventListener("click", onDelete);
+    li.innerText = todo.text;
+    li.id = todo.id;
+    ul.appendChild(li);
+    li.appendChild(removeBtn);
+  });
+};
+
+store.subscribe(() => {
+  store.getState();
+});
